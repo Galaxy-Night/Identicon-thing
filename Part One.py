4e857bd443f223348b802d4438cc3ba2003a9b8c
Liannium <meliaerinelizabeth@gmail.com>
2020-07-16 00:06:32 -0700

    Added horizontal symmetry

--- Part One.py
+++ Part One.py
@@ -7,14 +7,15 @@ hash = hashlib.sha1()
 hash.update(bytes)
 seed = int.from_bytes(hash.digest(), 'little')
 random.seed(seed)
-size = random.choice((2, 4, 8))
+size = random.choice((1, 2, 4))
 for i in range(0, size):
     row = ''
     for j in range(0, size):
         val = random.randrange(0, 2)
         if val is not 0:
-            row += '\u2588' * int(8/size) * 2
+            row += '\u2588' * int(4/size) * 2
         else:
-            row += ' ' * int(8/size) * 2
+            row += ' ' * int(4/size) * 2
+    row += row[::-1]
     for k in range(0, int(8/size)):
-        print(row.center(8))
+        print(row.center(size))
