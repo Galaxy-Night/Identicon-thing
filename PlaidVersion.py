30b5669261bddf0f24baf229163aed8b3e101b42
Liannium <meliaerinelizabeth@gmail.com>
2020-07-24 15:49:13 -0700

    Plaid version correctly handles transparency

--- PlaidVersion.py
+++ PlaidVersion.py
@@ -28,20 +28,24 @@ for i in range(0, numHorizontalStripes):
     leftside = random.randint(0, multiplier - 1)
     rightside = random.randrange(leftside, multiplier)
     color = random.choice(colors)
-    stripe = ImageDraw.Draw(output)
+    pasted = Image.new('RGBA', (320, 320), color='#ffffff00')
+    stripe = ImageDraw.Draw(pasted)
     for j in range(0, size):
         translation = j * multiplier
         stripe.rectangle((leftside + translation, 0, rightside + translation, 320), color)
-        output.save(f'{string}.png')
+    output.paste(pasted, (0, 0), pasted)
+    output.save(f'{string}.png')
 numVerticalStripes = random.randrange(1, 5)
 for i in range(0, numVerticalStripes):
     top = random.randint(0, multiplier - 1)
     bottom = random.randrange(top, multiplier)
     color = random.choice(colors)
-    stripe = ImageDraw.Draw(output)
+    pasted = Image.new('RGBA', (320, 320), color='#ffffff00')
+    stripe = ImageDraw.Draw(pasted)
     for j in range(0, size):
         translation = j * multiplier
         stripe.rectangle((0, top + translation, 320, bottom + translation), color)
-        output.save(f'{string}.png')
+    output.paste(pasted, (0, 0), pasted)
+    output.save(f'{string}.png')
 output.show()
 
